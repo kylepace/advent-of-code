f1e746d3bfbcda9028da9d13766954983b7658a5
kylepace <kylepace03@gmail.com>
2019-12-17 23:36:41 -0500

    Day3: Part 1

---
+++ src/main/scala/Day3.scala
@@ -0,0 +1,27 @@
+case class Point(x: Int, y: Int)
+
+object Day3 {
+  def placeWireStrand(start: Point, instruction: String): Vector[Point] = {
+    val direction = instruction(0)
+    val wireLength = instruction.substring(1).toInt
+
+    (direction match {
+      case 'R' => for (i <- start.y + 1 until wireLength + start.y + 1) yield Point(start.x, i)
+      case 'L' => for (i <- start.y + -1 until -wireLength - 1 + start.y by -1) yield Point(start.x, i)
+      case 'U' => for (i <- start.x + 1 until wireLength + 1 + start.x) yield Point(i, start.y)
+      case 'D' => for (i <- start.x + -1 until -wireLength - 1 + start.x by -1) yield Point(i, start.y)
+    }).toVector
+  }
+
+  def placeWires(instructions: List[String]): Vector[Point] = placeWires_h(instructions, Point(0, 0))
+
+  def getManhattanDistance(firstWire: Vector[Point], secondWire: Vector[Point]): Int =
+    firstWire.intersect(secondWire).map(x => Math.abs(x.x - 0) + Math.abs(x.y - 0)).min
+
+  private def placeWires_h(instructions: List[String], start: Point): Vector[Point] = instructions match {
+    case Nil => Vector.empty
+    case h :: t =>
+      val points = placeWireStrand(start, h)
+      points ++ placeWires_h(t, points.last)
+  }
+}
---
+++ src/test/scala/Day3Spec.scala
@@ -0,0 +1,67 @@
+import org.scalatest.FlatSpec
+import scala.io.Source
+
+class Day3Spec extends FlatSpec {
+  "place wire strand" should "place wire to the right from start" in {
+    val result = Day3.placeWireStrand(Point(0,0), "R4")
+    assert(result.contains(Point(0, 1)))
+    assert(result.contains(Point(0, 2)))
+    assert(result.contains(Point(0, 3)))
+    assert(result.contains(Point(0, 4)))
+  }
+
+  "place wire strand" should "place wire to the left from start" in {
+    val result = Day3.placeWireStrand(Point(0,0), "L4")
+    assert(result.contains(Point(0, -1)))
+    assert(result.contains(Point(0, -2)))
+    assert(result.contains(Point(0, -3)))
+    assert(result.contains(Point(0, -4)))
+  }
+
+  "place wire strand" should "place wire up from start" in {
+    val result = Day3.placeWireStrand(Point(0,0), "U4")
+    assert(result.contains(Point(1, 0)))
+    assert(result.contains(Point(2, 0)))
+    assert(result.contains(Point(3, 0)))
+    assert(result.contains(Point(4, 0)))
+  }
+
+  "place wire strand" should "place wire down from start" in {
+    val result = Day3.placeWireStrand(Point(0,0), "D4")
+    assert(result.contains(Point(-1, 0)))
+    assert(result.contains(Point(-2, 0)))
+    assert(result.contains(Point(-3, 0)))
+    assert(result.contains(Point(-4, 0)))
+  }
+
+  "place wires" should "place three wires in order" in {
+    val result = Day3.placeWires(List("U2", "R2", "D2"))
+    assert(result.contains(Point(1, 0)))
+    assert(result.contains(Point(2, 0)))
+
+    assert(result.contains(Point(2, 1)))
+    assert(result.contains(Point(2, 2)))
+
+    assert(result.contains(Point(1, 2)))
+    assert(result.contains(Point(0, 2)))
+  }
+
+  "get manhattan distance" should "get correct manhattan distance" in {
+    val firstWire = Day3.placeWires(List("R8", "U5", "L5", "D3"))
+    val secondWire = Day3.placeWires(List("U7", "R6", "D4", "L4"))
+    val result = Day3.getManhattanDistance(firstWire, secondWire)
+
+    assert(result == 6)
+  }
+
+  "calculate total manhattan distance" should "return my personal puzzle result" in {
+    val day3File = Source.fromURL(getClass.getResource("Day3Input.txt"))
+    val wires = day3File.getLines().map(s => s.split(',').toList).toList
+    val firstWire = Day3.placeWires(wires.head)
+    val secondWire = Day3.placeWires(wires.last)
+
+    val result = Day3.getManhattanDistance(firstWire, secondWire)
+
+    assert(result == 529)
+  }
+}
